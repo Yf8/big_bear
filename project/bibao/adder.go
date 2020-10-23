e84299af5c3cf97df6038d115666a412144a47e9
Yf8 <469216710@qq.com>
2020-10-23 16:29:17 +0800

    go code

---
+++ project/bibao/adder.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func adder() func (int) int {
+	sum := 0
+	return func (v int) int {
+		sum += v
+		fmt.Printf("current value : %d\n", sum)
+		return sum
+	}
+}
+
+func main()  {
+	a := adder()
+	//for i := 0; i < 10; i++ {
+	//	fmt.Printf("0 + 1 + ... + %d = %d \n",i, a(i))
+	//}
+	fmt.Printf("%d", a(5))
+}
